6d57b2500e1ea3671e4970d8e2a6c31c4bc1ae2b
Mateusz Okoń <mateusz.okon04@gmail.com>
2025-09-04 08:15:35 +0200

    Navbar complete

--- portfolio/src/components/Navbar/Navbar.jsx
+++ portfolio/src/components/Navbar/Navbar.jsx
@@ -1,26 +1,27 @@
 import React, {useState} from 'react';
 import styles from "./Navbar.module.css";
-import {getUrl} from "../../utils";
+import openMenuIcon from '../../assets/Navbar/openMenuIcon.svg';
+import closeMenuIcon from '../../assets/Navbar/closeMenuIcon.svg';
 
 export const Navbar = () => {
+    const [menuOpen, setMenuOpen] = useState(false);
   return (
     <nav className={styles.navbar}>
         <a className={styles.title} href = '/'>Portfolio</a>
         <div className={styles.menu}>
-            <img className={styles.menuBtn} src={getUrl("Navbar/menuicon.png")} alt="icon"/>
-            <ul className={styles.menuItems}>
-                <li>
-                    <a href='#about'>O mnie</a>
-                </li>
-                <li>
-                    <a href='#projects'>Projekty</a>
-                </li>
-                <li>
-                    <a href='#education'>Edukacja i Doświadczenie</a>
-                </li>
-                <li>
-                    <a href='#contact'>Kontakt</a>
-                </li>
+            <img className={styles.menuBtn} 
+            src={menuOpen ? closeMenuIcon : openMenuIcon} 
+            alt="icon"
+            onClick={() => setMenuOpen(!menuOpen)}
+            />
+
+            <ul className={`${styles.menuItems} ${menuOpen && styles.menuOpen}`}
+            onClick={()=>setMenuOpen(!menuOpen)}>
+            
+                <li><a href="#about">O mnie</a></li>
+                <li><a href="#projects">Projekty</a></li>
+                <li><a href="#education">Edukacja i Doświadczenie</a></li>
+                <li><a href="#contact">Kontakt</a></li>
             </ul>
         </div>
     </nav>
--- portfolio/src/utils.js
+++
@@ -1,3 +0,0 @@
-export const getUrl = (path) => {
-  return new URL(`../assets/${path}`, import.meta.url).href;
-};
