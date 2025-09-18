118a1d9f41d255d8127555c5ef72576fc7dea51d
Mateusz Okoń <mateusz.okon04@gmail.com>
2025-09-18 14:04:58 +0200

    add deploy workflow

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/portfolio-website/',
   plugins: [react()],
   css: {
-    modules:{
-      localsConvention : "camelCase",
+    modules: {
+      localsConvention: 'camelCase',
     },
   },
-});
+})
